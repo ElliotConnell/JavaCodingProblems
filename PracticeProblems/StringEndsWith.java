f821ca8974bc12f8e853d2e82d9c243b8c5d0399
Elliot Connell <elliot.connell@gmail.com>
2021-06-11 14:59:29 +1000

    StringEndsWith first commit

---
+++ PracticeProblems/StringEndsWith.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of StringEndsWith here.
+ * 
+ * Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+ * Examples:
+
+ * strEndsWith('abc', 'bc') -- returns true
+ * strEndsWith('abc', 'd') -- returns false
+ * 
+ * @author (Elliot Connell) 
+ * @version (a version number or a date)
+ */
+public class StringEndsWith {
+    
+    private boolean strEndsWith(String string, String str) {
+        
+        
+        return false;
+    }
+
+}
